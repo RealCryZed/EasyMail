7dc4c80813f6e7dc75bc6b67c7f9674d48f2d6ec
RealCryZed <bujhmgtnhjd@bk.ru>
2019-07-16 20:16:11 +0600

    Added MainController and mainPage.fxml, Added option to send mails (bugged)

--- src/main/java/com/easymail/App.java
+++ src/main/java/com/easymail/App.java
@@ -1,9 +1,29 @@
 package com.easymail;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Parent root = FXMLLoader.load((getClass().getResource("com.easymail/mainPage.fxml")));
+        Scene scene = new Scene(root);
+
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+//        primaryStage.setResizable(false);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        launch();
     }
 }
